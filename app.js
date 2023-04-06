d309d511720d71e90b0a8aba0ae5740e28330c16
plebg <plebeiusgaragicus@gmail.com>
2023-04-05 23:50:31 -0700

    bot.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
+
 import { Telegraf } from 'telegraf';
+
+import { setupBot } from './bot.js';
 
 dotenv.config();
 
@@ -47,118 +50,12 @@ app.listen(PORT, () => {
 });
 
 
-
 //// INIT THE BOT ////
 export const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-
-//// /START COMMAND ////
-import { ShowTopLevelCommands } from './helpers.js';
-bot.command('start', async ctx => {
-    console.log("start command called - CHAT ID: ", ctx.chat.id, " - FROM: ", ctx.from.username, "User ID: ", ctx.from.id);
-    ctx.deleteMessage();
-
-    ShowTopLevelCommands(ctx)
-});
-
-import { mode_callback } from './helpers.js';
-
-// NOTE: THIS NEEDS TO BE LAST!!!
-// TODO: turn this into a modal kind of thing... where the user can enter messages (that will be deleted).. but the context is whatever inline keyboard is currently showing.  Easy to set a global where the bot keeps track of which "mode" it is in.
-// This is a catch-all for any messages that are not commands.
-bot.on('message', ctx => {
-    if (mode_callback == null) {
-        // bot.telegram.sendMessage(ctx.message.chat.id, "WARNING: I only respond to commands.  Please use /help to see a list of commands.");
-        ctx.reply("WARNING: I only respond to commands.  You can always /start again if you need.");
-        return;
-    }
-
-    mode_callback(ctx);
-
-})
-
-
-
-// THIS IS THE 'BACK BUTTON'
-bot.action('show_commands', async ctx => {
-    ctx.deleteMessage();
-    ShowTopLevelCommands(ctx)
-});
-
-
-//// OLD WAY OF TEACHING THE BOT NEW COMMANDS ////
-// require('./commands/braiins_API').activate(bot);
-// require('./commands/bitcoin').activate(bot);
-
-
-//// NEW WAY OF TEACHING THE BOT NEW COMMANDS - THAT KEEPS APP.JS TIDY ////
-import { teachBitcoin } from './commands/bitcoin.js';
-teachBitcoin(bot);
-
-import { teachBraiins } from './commands/braiins.js';
-teachBraiins(bot);
-
-import { teachMiner } from './commands/miner.js';
-teachMiner(bot);
+setupBot(bot);
 
 
 // START THE BOT AND SAY HELLO
 bot.launch();
 console.log("Bot started");
-
-async function getBotDetails() {
-    try {
-        const botInfo = await bot.telegram.getMe();
-        console.log('Bot Details:', botInfo);
-    } catch (error) {
-        console.error('Error getting bot details:', error);
-    }
-}
-
-getBotDetails();
-
-
-
-
-//// TODO: I'm not sure this is needed...
-//// Send a message to the chat id that the bot is running on
-//// ... it ensures the bot will only chat with the 
-// const targetChatId = process.env.CHAT_ID || undefined
-// if (targetChatId === undefined) {
-//     console.error("CHAT ID is not setup in .env\nuse \/get_chat_id then use the web portal to save it.")
-//     // process.exit(1)
-// }
-// else
-
-
-
-//// 'MIDDLEWARE' THAT RESTRICTS WHICH USER THE BOT WILL RESPOND TO ////
-// TODO: I NEED TO TEST THIS!!!
-const ALLOWED_USER_ID = process.env.CHAT_ID || undefined
-if (ALLOWED_USER_ID === undefined) {
-    console.log(">>> WARNING!!!\n>>>\tCHAT_ID not set.  Please set this in the .env file.\n>>>\tThis is the Telegram user ID that will be allowed to use this bot.\n>>>\tYou can find your user ID by sending a message to @userinfobot\n>>>\n");
-} else {
-    bot.use((ctx, next) => {
-        if (ctx.from.id.toString() === ALLOWED_USER_ID) {
-            return next();
-        } else {
-            console.log(`Unauthorized access attempt by user ID: ${ctx.from.id}`);
-            // ctx.reply(`Sorry, you are not authorized to use this bot.`);
-        }
-    });
-    //// SAY HELLO
-    bot.telegram.sendMessage(ALLOWED_USER_ID, `Hello,\nI'm awake and ready to /start`);
-
-    process.on('SIGINT', () => {
-        console.log("SIGINT");
-        bot.telegram.sendMessage(ALLOWED_USER_ID, `(X.X) I'M DYING - SIGINT!!!`)
-            .then(() => {
-                console.log("process exiting...");
-                process.exit(0);
-            })
-            .catch((err) => {
-                console.error(err);
-                process.exit(1); // exit with error status
-            });
-    });
-}
---
+++ bot.js
@@ -0,0 +1,105 @@
+import { ShowTopLevelCommands, mode_callback } from './helpers.js';
+
+
+//// OLD WAY OF TEACHING THE BOT NEW COMMANDS ////
+// require('./commands/braiins_API').activate(bot);
+// require('./commands/bitcoin').activate(bot);
+import { teachBitcoin } from './commands/bitcoin.js';
+import { teachBraiins } from './commands/braiins.js';
+import { teachMiner } from './commands/miner.js';
+
+
+
+
+
+
+export async function setupBot(bot) {
+
+    try {
+        const botInfo = await bot.telegram.getMe();
+        console.log('Bot Details:', botInfo);
+    } catch (error) {
+        console.error('Error getting bot details:', error);
+    }
+
+
+    bot.command('start', async ctx => {
+        console.log("start command called - CHAT ID: ", ctx.chat.id, " - FROM: ", ctx.from.username, "User ID: ", ctx.from.id);
+        ctx.deleteMessage();
+
+        ShowTopLevelCommands(ctx)
+    });
+
+    bot.action('show_commands', async ctx => {
+        ctx.deleteMessage();
+        ShowTopLevelCommands(ctx)
+    });
+
+
+    // TEACH THE BOT EACH `SKILL`
+    teachBitcoin(bot);
+    teachBraiins(bot);
+    teachMiner(bot);
+
+
+    // NOTE: THIS NEEDS TO BE LAST!!!
+    // TODO: turn this into a modal kind of thing... where the user can enter messages (that will be deleted).. but the context is whatever inline keyboard is currently showing.  Easy to set a global where the bot keeps track of which "mode" it is in.
+    // This is a catch-all for any messages that are not commands.
+    bot.on('message', ctx => {
+        if (mode_callback == null) {
+            // bot.telegram.sendMessage(ctx.message.chat.id, "WARNING: I only respond to commands.  Please use /help to see a list of commands.");
+            ctx.reply("WARNING: I only respond to commands.  You can always /start again if you need.");
+            return;
+        }
+
+        mode_callback(ctx);
+
+    })
+
+
+
+
+
+
+
+    //// TODO: I'm not sure this is needed...
+    //// Send a message to the chat id that the bot is running on
+    //// ... it ensures the bot will only chat with the 
+    // const targetChatId = process.env.CHAT_ID || undefined
+    // if (targetChatId === undefined) {
+    //     console.error("CHAT ID is not setup in .env\nuse \/get_chat_id then use the web portal to save it.")
+    //     // process.exit(1)
+    // }
+    // else
+
+    //// 'MIDDLEWARE' THAT RESTRICTS WHICH USER THE BOT WILL RESPOND TO ////
+    // TODO: I NEED TO TEST THIS!!!
+    const ALLOWED_USER_ID = process.env.CHAT_ID || undefined
+    if (ALLOWED_USER_ID === undefined) {
+        console.log(">>> WARNING!!!\n>>>\tCHAT_ID not set.  Please set this in the .env file.\n>>>\tThis is the Telegram user ID that will be allowed to use this bot.\n>>>\tYou can find your user ID by sending a message to @userinfobot\n>>>\n");
+    } else {
+        bot.use((ctx, next) => {
+            if (ctx.from.id.toString() === ALLOWED_USER_ID) {
+                return next();
+            } else {
+                console.log(`Unauthorized access attempt by user ID: ${ctx.from.id}`);
+                // ctx.reply(`Sorry, you are not authorized to use this bot.`);
+            }
+        });
+        //// SAY HELLO
+        bot.telegram.sendMessage(ALLOWED_USER_ID, `Hello,\nI'm awake and ready to /start`);
+
+        process.on('SIGINT', () => {
+            console.log("SIGINT");
+            bot.telegram.sendMessage(ALLOWED_USER_ID, `(X.X) I'M DYING - SIGINT!!!`)
+                .then(() => {
+                    console.log("process exiting...");
+                    process.exit(0);
+                })
+                .catch((err) => {
+                    console.error(err);
+                    process.exit(1); // exit with error status
+                });
+        });
+    }
+}
